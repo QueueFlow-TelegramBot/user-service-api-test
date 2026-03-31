13065eb1a609f6891d1235e05476dbb362f4193c
muffindud <ccatlabuga2002@gmail.com>
2026-03-31 11:07:13 +0300

    add existing users test

---
+++ src/test/java/com/github/queueflow/BaseTest.java
@@ -0,0 +1,5 @@
+package com.github.queueflow;
+
+public abstract class BaseTest {
+    public static final String BASE_URI = "http://192.168.0.139:31858/";
+}
--- src/test/java/com/github/queueflow/ExistingUserTest.java
+++ src/test/java/com/github/queueflow/ExistingUserTest.java
@@ -2,13 +2,17 @@ package com.github.queueflow;
 
 import com.github.javafaker.Faker;
 import com.github.queueflow.dto.User;
+import io.restassured.http.ContentType;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.Random;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
-public class ExistingUserTest {
+
+public class ExistingUserTest extends BaseTest {
     public static User user;
 
     @BeforeTest
@@ -17,31 +21,77 @@ public class ExistingUserTest {
                 new Faker(new Random()).number().digits(9),
                 new Faker(new Random()).name().firstName()
         );
+
+        given()
+                .contentType(ContentType.JSON)
+                .baseUri(BASE_URI)
+                .body(ExistingUserTest.user)
+        .when()
+                .post("/user")
+        .then()
+                .statusCode(201);
     }
 
     @Test
     public static void getExistingUserByTelegramIdTest() {
-
+        given()
+                .contentType(ContentType.JSON)
+                .baseUri(BASE_URI)
+                .pathParam("telegram_id", ExistingUserTest.user.telegramId)
+        .when()
+                .get("/user/{telegram_id}")
+        .then()
+                .body("telegram_id", equalTo(ExistingUserTest.user.telegramId))
+                .body("display_name", equalTo(ExistingUserTest.user.displayName))
+                .statusCode(200);
     }
 
     @Test
     public static void getNonexistingUserByTelegramId() {
-
+        given()
+                .contentType(ContentType.JSON)
+                .baseUri(BASE_URI)
+                .pathParam("telegram_id", new Faker(new Random()).number().digits(9))
+        .when()
+                .get("/user/{telegram_id}")
+        .then()
+                .statusCode(404);
     }
 
     @Test
     public static void createExistingUserTokenTest() {
-
+        given()
+                .contentType(ContentType.JSON)
+                .baseUri(BASE_URI)
+                .queryParam("telegram_id", ExistingUserTest.user.telegramId)
+        .when()
+                .post("/user/token")
+        .then()
+                .statusCode(200);
     }
 
     @Test
     public static void createNonexistingUserTokenTest() {
-
+        given()
+                .contentType(ContentType.JSON)
+                .baseUri(BASE_URI)
+                .queryParam("telegram_id", new Faker(new Random()).number().digits(9))
+        .when()
+                .post("/user/token")
+        .then()
+                .statusCode(404);
     }
 
 
     @Test
     public static void createExistingValidUserTest() {
-
+        given()
+                .contentType(ContentType.JSON)
+                .baseUri(BASE_URI)
+                .body(ExistingUserTest.user)
+        .when()
+                .post("/user")
+        .then()
+                .statusCode(409);
     }
 }
